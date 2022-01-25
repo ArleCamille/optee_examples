9e6f7cbf1b5316831c8cafed77f398a43c3e03f0
Hyeseon Oh <33932097+ArleCamille@users.noreply.github.com>
2022-01-25 10:40:07 +0900

    Updated host code to integrate OpenCL

--- sopencl_hello_world/host/main.c
+++ sopencl_hello_world/host/main.c
@@ -16,8 +16,18 @@
 /* For the UUID (found in the TA's h-file(s)) */
 #include <plugin_ta.h>
 
+/* OpenCL */
+#ifdef __APPLE__
+#include <OpenCL/opencl.h>
+#else
+#include <CL/cl.h>
+#endif
+
 #define SLEEP_SEC 2
 #define TA_PING_CNT 5
+
+#define MEM_SIZE (128)
+#define MAX_SOURCE_SIZE (0x100000)
 
 int main(void)
 {
@@ -28,12 +38,78 @@ int main(void)
 	TEEC_Operation op = { };
 	TEEC_UUID uuid = PLUGIN_TA_UUID;
 	uint32_t err_origin = 0;
+	
+	/* OpenCL variables */
+	cl_device_id device_id = NULL;
+	cl_context context = NULL;
+	cl_command_queue command_queue = NULL;
+	cl_mem memobj = NULL;
+	cl_program program = NULL;
+	cl_kernel kernel = NULL;
+	cl_platform_id platform_id = NULL;
+	cl_uint ret_num_devices;
+	cl_uint ret_num_platforms;
+	cl_int ret;
+	
+	char string[MEM_SIZE];
+	
+	FILE *fp;
+	char fileName[] = "./hello.cl";
+	char *source_str;
+	size_t source_size;
+	
+	void *gpumem_host;
+	
+	/* OpenCL initialization */
+	/* Load the source code containing the kernel */
+	fp = fopen (fileName, "r");
+	if (!fp)
+	{
+		fprintf (stderr, "Failed to load kernel\n");
+		return 1;
+	}
+	source_str = (char*) malloc (MAX_SOURCE_SIZE);
+	source_size = fread (source_str, 1, MAX_SOURCE_SIZE, fp);
+	fclose (fp);
+	
+	/* Create OpenCL context */
+	context = clCreateContext (NULL, 1, &device_id, NULL, NULL, &ret);
+	
+	/* Create command queue */
+	command_queue = clCreateCommandQueue (context, device_id, 0, &ret);
+	
+	/* Create memory buffer at the GPU end */
+	memobj = clCreateBuffer (context, CL_MEM_READ_WRITE, MEM_SIZE * sizeof (char), NULL, &ret);
+	
+	/* Map created buffer */
+	gpumem_host = clEnqueueMapBuffer (command_queue, memobj, CL_TRUE, CL_MAP_READ | CL_MAP_WRITE,
+					  0, MEM_SIZE * sizeof (char), 0, NULL, NULL, &ret);
+	
+	/* Create kernel program from the source */
+	program = clCreateProgramWithSource (context, 1, (const char **)&source_str,
+		(const size_t *)&source_size, &ret);
+	
+	/* Build kernel program */
+	ret = clBuildProgram (program, 1, &device_id, NULL, NULL, NULL);
+	
+	/* Create OpenCL kernel */
+	kernel = clCreateKernel (program, "hello", &ret);
+	
+	/* Set OpenCL kernel parameters */
+	ret = clSetKernelArg (kernel, 0, sizeof (cl_mem), (void *)&memobj);
 
+	/* TEE initialization */
 	/* Initialize a context connecting us to the TEE */
 	res = TEEC_InitializeContext(NULL, &ctx);
 	if (res != TEEC_SUCCESS)
 		errx(1, "TEEC_InitializeContext failed with code %#" PRIx32,
 		     res);
+	
+	/* Allocate CL memory as shared */
+	TEEC_SharedMemory shm = {0};
+	shm.flags = TEEC_MEM_INPUT | TEEC_MEM_OUTPUT;
+	shm.size = MEM_SIZE;
+	shm.buffer = gpumem_host;
 
 	/* Open a session to the "plugin" TA */
 	res = TEEC_OpenSession(&ctx, &sess, &uuid, TEEC_LOGIN_PUBLIC, NULL,
@@ -45,7 +121,20 @@ int main(void)
 	/* Clear the TEEC_Operation struct */
 	memset(&op, 0, sizeof(op));
 	op.paramTypes =
-		TEEC_PARAM_TYPES(TEEC_NONE, TEEC_NONE, TEEC_NONE, TEEC_NONE);
+		TEEC_PARAM_TYPES(TEEC_MEMREF_WHOLE, TEEC_NONE, TEEC_NONE, TEEC_NONE);
+	op.params[0].memref.parent = &shm;
+	op.params[0].memref.size = shm.size;
+	
+	if (TEEC_RegisterSharedMemory (&ctx, &shm) != TEEC_SUCCESS)
+		goto main_err;
+	
+	/* Back to OpenCL... */
+	/* Enqueue OpenCL kernel */
+	ret = clEnqueueTask (command_queue, kernel, 0, NULL, NULL);
+	
+	/* Finish Current commands */
+	ret = clFlush (command_queue);
+	ret = clFinish (command_queue);
 
 	/*
 	 * TA will refer to the syslog plugin to print some log messages to REE.
@@ -57,17 +146,14 @@ int main(void)
 	printf("Work logic: REE --> plugin TA --> syslog plugin in REE --> syslog\n");
 	printf("See the log from TEE through 'journalctl'\n\n");
 
-	for (i = 0; i < TA_PING_CNT; ++i) {
-		res = TEEC_InvokeCommand(&sess, PLUGIN_TA_PING, &op,
-					 &err_origin);
+	res = TEEC_InvokeCommand(&sess, PLUGIN_TA_PING, &op,
+				 &err_origin);
 
-		printf("Attempt #%d: TEEC_InvokeCommand() %s; res=%#" PRIx32 " orig=%#" PRIx32 "\n",
-		       i + 1, (res == TEEC_SUCCESS) ? "success" : "failed",
-		       res, err_origin);
+	printf("Attempt: TEEC_InvokeCommand() %s; res=%#" PRIx32 " orig=%#" PRIx32 "\n",
+	       (res == TEEC_SUCCESS) ? "success" : "failed",
+	       res, err_origin);
 
-		sleep(SLEEP_SEC);
-	}
-
+	main_err:
 	/*
 	 * We're done with the TA, close the session and
 	 * destroy the context.
@@ -75,6 +161,15 @@ int main(void)
 
 	TEEC_CloseSession(&sess);
 	TEEC_FinalizeContext(&ctx);
+	
+	/* OpenCL cleanup */
+	ret = clReleaseKernel (kernel);
+	ret = clReleaseProgram (program);
+	ret = clReleaseMemObject (memobj);
+	ret = clReleaseCommandQueue (command_queue);
+	ret = clReleaseContext (context);
+	
+	free (source_str);
 
 	return 0;
 }
